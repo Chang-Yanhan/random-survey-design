25f196ab6c079fbd174e9b612b86bc18da18e4c2
Chang-Yanhan <59954063+Chang-Yanhan@users.noreply.github.com>
2023-03-24 16:15:58 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,8 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://www.figma.com/proto/7wa6N6udbQVUx17osN4Let/S1-B?page-id=0%3A1&node-id=1-13&viewport=519%2C374%2C0.45&scaling=min-zoom&starting-point-node-id=1%3A13',
+	'https://www.figma.com/proto/XZwYTwKVXcEl24fUW32f6o/S1-F?page-id=0%3A1&node-id=1-105&viewport=1786%2C516%2C0.95&scaling=min-zoom&starting-point-node-id=1%3A105',
+	'https://www.figma.com/proto/Cbd0fuhqx02RFCWXM136Mq/S2-T?page-id=0%3A1&node-id=1-59&viewport=988%2C410%2C0.15&scaling=min-zoom&starting-point-node-id=1%3A59',
+	'https://www.figma.com/proto/KCKlfvIBnUSQN0esYu34NQ/S2-W?page-id=0%3A1&node-id=1-5261&viewport=1600%2C393%2C0.2&scaling=min-zoom&starting-point-node-id=1%3A5261'
 ];
